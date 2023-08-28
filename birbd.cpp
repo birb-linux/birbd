eac0206b7d58955d0c53323ea59785b2421a13ac
Toasterbirb <git@toasterbirb.com>
2023-08-28 17:00:02 +0300

    Make the server IP configurable

--- birbd.cpp
+++ birbd.cpp
@@ -4,25 +4,25 @@
 
 int main(int argc, char** argv)
 {
-	if (argc == 2)
+	/* Act as a server */
+	if (argc == 2 && !strcmp("--server", argv[1]))
 	{
-		/* Act as a server */
-		if (!strcmp("--server", argv[1]))
-		{
-			Server server(1500);
-			server.listen();
-			return 0;
-		}
+		Server server(1500);
+		server.listen();
+		return 0;
+	}
 
-		/* Send a message to the server */
-		Client client("localhost", "1500");
-		std::string response = client.connect(argv[1]);
+	/* Send a message to the server */
+	if (argc == 3)
+	{
+		Client client(argv[1], "1500");
+		std::string response = client.connect(argv[2]);
 		std::cout << response << std::endl;
 	}
 	else
 	{
-		std::cout 	<< "Usage: birbd [--server|message]\n\n"
-					<< "If --server option is used, birbd will act as a server\n\n"
+		std::cout 	<< "Usage: birbd [ip address] [--server|message]\n\n"
+					<< "If --server option is used, birbd will act as a server. IP-address isn't needed in server-mode\n\n"
 					<< "Valid messages (values between [] are placeholders):\n"
 					<< "  ping                                       the server should answer with pong\n"
 					<< "  has_package;[tarball_name];[md5_checksum]  the server will answer either positive or negative\n"
