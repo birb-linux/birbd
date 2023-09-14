a0de9ac40cdd45b91a237ed5c8d1cf0c456bc82c
Toasterbirb <git@toasterbirb.com>
2023-09-14 17:13:05 +0300

    Stop using the deprecated MD5 function

--- crypto.cpp
+++ crypto.cpp
@@ -1,18 +1,28 @@
-#define OPENSSL_NO_DEPRECATED_3_0
 #include "Crypto.hpp"
 #include <fstream>
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 
 std::string md5sum(const std::string& data)
 {
-	unsigned char result_chararr[MD5_DIGEST_LENGTH];
-	MD5((unsigned char*)data.c_str(), data.length(), result_chararr);
+	/* Some boilerplate politely stolen partially
+	 * from https://stackoverflow.com/a/73843621 */
+	EVP_MD_CTX* context = EVP_MD_CTX_new();
+	const EVP_MD* md = EVP_md5();
+	unsigned char md_value[EVP_MAX_MD_SIZE];
+	unsigned int md_len;
+
+	EVP_DigestInit_ex(context, md, NULL);
+	EVP_DigestUpdate(context, data.c_str(), data.size());
+	EVP_DigestFinal_ex(context, md_value, &md_len);
+	EVP_MD_CTX_free(context);
 
 	std::string result;
+	result.reserve(md_len * 2);
+
 	char buffer[sizeof(int)];
-	for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
+	for (int i = 0; i < md_len; i++)
 	{
-		snprintf(buffer, sizeof(int), "%02x", result_chararr[i]);
+		snprintf(buffer, sizeof(int), "%02x", md_value[i]);
 		result += buffer;
 	}
 	return result;
